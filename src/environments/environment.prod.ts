95b3d34d0f9be7f5d380aa652c9d4c2bfb728653
ramphy0x90 <ramphy11@hotmail.com>
2025-03-22 09:31:31 +0100

    min: envs

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    athenaAuthApi: "https://auth.r16a.cloud/api/v1"
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    athenaAuthApi: "http://localhost:8080/api/v1"
+}
